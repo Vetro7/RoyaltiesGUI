9e1d6dd67e422e3c83d64dbb75fd4822c22789a5
Vetro7 <ericvesprini@yahoo.com>
2017-04-25 17:41:33 -0400

    Update CryptoNoteWallet.cmake

--- CryptoNoteWallet.cmake
+++ CryptoNoteWallet.cmake
@@ -1,5 +1,5 @@
 
-set(CN_PROJECT_NAME "bitcediwallet")
-set(CN_CURRENCY_DISPLAY_NAME "Bitcedi")
-set(CN_CURRENCY_TICKER "BXC")
+set(CN_PROJECT_NAME "royaltieswallet")
+set(CN_CURRENCY_DISPLAY_NAME "Royalties")
+set(CN_CURRENCY_TICKER "XRY")
 set(CN_VERSION 0.1.0)
